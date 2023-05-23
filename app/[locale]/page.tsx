1d21876881d8568923bd1229674a8518fa403e31
trebordng <96870545+trebordng@users.noreply.github.com>
2023-05-23 17:06:32 +1000

    add background

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,19 +1,18 @@
 import Animation from "@/layout/animation";
 import Image from "next/image";
-import home from "@/public/images/home.jpg"
+import home from "@/public/images/home.jpg";
 export default function Home() {
+  console.log(home.blurDataURL);
   return (
-    <Animation>
-      <Image
-        src={home.src}
-        placeholder="blur"
-        blurDataURL={home.blurDataURL}
-        sizes="auto"
-        width={100}
-        height={100}
-        alt="Cabbage with Sesame Seeds"
-        className="h-full w-full object-cover"
-      ></Image>
-    </Animation>
+    <Image
+      src={home.src}
+      placeholder="blur"
+      blurDataURL={home.blurDataURL}
+      sizes="auto"
+      width={100}
+      height={100}
+      alt="Cabbage with Sesame Seeds"
+      className="h-full w-full object-cover"
+    ></Image>
   );
 }
