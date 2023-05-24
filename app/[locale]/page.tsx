26b6ddf00fe15d75c94d81ce7f9bb815a4c2def6
trebordng <96870545+trebordng@users.noreply.github.com>
2023-05-24 15:37:04 +1000

    add album list

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -23,31 +23,29 @@ export default function Album() {
   ];
 
   return (
-    <Animation>
-      <section className="flex flex-col lg:flex-wrap lg:flex-row gap-16 md:gap-24 lg:gap-32 xl:gap-40 h-full overflow-x-hidden">
-        {albums.map((album: Album) => (
-          <Link
-            href={album.slug}
-            key={album.slug}
-            className="lg:w-[calc(50%-20px)] lg:h-[calc(50%-20px)] lg:max-h-[50vh] rounded-lg overflow-hidden relative bg-black hover:bg-none"
-          >
-            <Image
-              src={album.image.src}
-              placeholder="blur"
-              loading="lazy"
-              blurDataURL={album.image.blurDataURL}
-              sizes="auto"
-              width={100}
-              height={100}
-              alt="Cabbage with Sesame Seeds"
-              className="h-full w-full object-cover hover:blur-sm transition opacity-80 hover:opacity-1"
-            />
-            <h2 className="text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl uppercase font-bold">
-              {album.name}
-            </h2>
-          </Link>
-        ))}
-      </section>
-    </Animation>
+    <section className="flex flex-col lg:flex-wrap lg:flex-row gap-16 md:gap-24 lg:gap-32 xl:gap-40 h-full overflow-x-hidden">
+      {albums.map((album: Album) => (
+        <Link
+          href={album.slug}
+          key={album.slug}
+          className="lg:w-[calc(50%-20px)] lg:h-[calc(50%-20px)] lg:max-h-[50vh] rounded-lg overflow-hidden relative bg-black hover:bg-none"
+        >
+          <Image
+            src={album.image.src}
+            placeholder="blur"
+            loading="lazy"
+            blurDataURL={album.image.blurDataURL}
+            sizes="auto"
+            width={100}
+            height={100}
+            alt="Cabbage with Sesame Seeds"
+            className="h-full w-full object-cover hover:blur-sm transition opacity-80 hover:opacity-1"
+          />
+          <h2 className="text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl uppercase font-bold">
+            {album.name}
+          </h2>
+        </Link>
+      ))}
+    </section>
   );
 }
