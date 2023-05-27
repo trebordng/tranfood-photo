f18fa9bee67ee2be2ba031706aaad42550190673
trebordng <96870545+trebordng@users.noreply.github.com>
2023-05-28 02:43:04 +1000

    update firebase post

--- components/uploadImage/ImageList.tsx
+++ components/uploadImage/ImageList.tsx
@@ -17,14 +17,14 @@ const ImageList: React.FC<ImageList> = ({ list }) => {
   return (
     <article className="flex flex-col md:flex-wrap md:flex-row gap-16 md:gap-24 flex-auto overflow-x-hidden">
       {list?.map((image: ImageObject,index) => (
-        <div key={image.title} className="md:w-[calc(50%-12px)] lg:w-[calc(33.33%-16px)] min-h-416 xl:min-h-512 rounded-lg overflow-hidden relative bg-black hover:bg-none">
+        <div key={image.title} className="md:w-[calc(50%-12px)] lg:w-[calc(33.33%-16px)] min-h-416 xl:min-h-512 rounded-lg overflow-hidden relative">
           <Image
             src={typeof image.url === "string" ? image.url : ""}
             alt={image.title}
             fill
             sizes="(max-width: 1024px) 100vw 100vh, 100%"
             style={{ objectFit: "cover" }}
-            className="h-full w-full hover:blur-sm transition opacity-80 hover:opacity-1"
+            className="h-full w-full hover:blur-sm transition"
             placeholder="blur"
             loading="lazy"
             blurDataURL={image?.blurDataURL}
