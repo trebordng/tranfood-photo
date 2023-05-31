ed66bd7aa4d5a6ec8eefe6f2e1e922c5c7d650fb
trebordng <96870545+trebordng@users.noreply.github.com>
2023-06-01 05:05:14 +1000

    update quality

--- components/album/Images.tsx
+++ components/album/Images.tsx
@@ -24,6 +24,7 @@ const Images: React.FC<Images> = ({ data }) => {
             src={image.url}
             alt={image.title}
             fill
+            quality={100}
             sizes="(max-width: 640px) 100%,
             (max-width: 1280px) 50%,
             (max-width: 1536px) 33%,
