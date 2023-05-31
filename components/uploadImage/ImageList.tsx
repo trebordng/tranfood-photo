799b74fd3da3b888c2db9caa5b9e2c9ecfa2cdb8
trebordng <96870545+trebordng@users.noreply.github.com>
2023-05-31 22:40:38 +1000

    update metatag

--- components/uploadImage/ImageList.tsx
+++ components/uploadImage/ImageList.tsx
@@ -62,7 +62,8 @@ const ImageList: React.FC<ImageList> = ({
             fill
             sizes="(max-width: 640px) 100%,
             (max-width: 1280px) 50%,
-            (max-width: 1536px) 33%"
+            (max-width: 1536px) 33%,
+            25vw"
             style={{ objectFit: "cover" }}
             className="hover:blur-sm transition peer"
             placeholder="blur"
