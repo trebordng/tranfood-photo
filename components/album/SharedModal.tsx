c34582b386b8953ccb82d4af8290745ea6f0ac34
trebordng <96870545+trebordng@users.noreply.github.com>
2023-06-06 01:22:25 +1000

    finalise onclose

--- components/album/SharedModal.tsx
+++ components/album/SharedModal.tsx
@@ -111,10 +111,7 @@ const SharedModal: React.FC<SharedModal> = ({
             <Image
               alt={data[index].title}
               src={data[index].url}
-              sizes="(max-width: 640px) 100vw,
-              (max-width: 1280px) 50%,
-              (max-width: 1536px) 33%,
-              25vw"
+              sizes="100vw"
               height={data[index].height}
               width={data[index].width}
               placeholder="blur"
